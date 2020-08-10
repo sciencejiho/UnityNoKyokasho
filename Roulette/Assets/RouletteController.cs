ffebca3e2a812ea481d3a241f3c6fbb5641ca6f5
JiHo Han <sciencejiho@gmail.com>
2020-08-11 03:50:51 +0900

    build(Roulette): add C# Script for rotating the roulette

--- Roulette/Assets/RouletteController.cs
+++ Roulette/Assets/RouletteController.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class RouletteController : MonoBehaviour
 {
+    // Rotation speed of the roulette
+    float rotSpeed = 0;
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
-
+    
     // Update is called once per frame
     void Update()
     {
-        
+        // Receive mouse click to manage the rotation speed
+        if(Input.GetMouseButtonDown(0))
+        {
+            this.rotSpeed = 10;
+        }
+
+    // Rotate the roulette based on the rotation speed
+    transform.Rotate(0, 0, this.rotSpeed);
     }
 }
