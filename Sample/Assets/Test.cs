5b703265dfe653580b6f44eb42687c9e8215403f
JiHo Han <sciencejiho@gmail.com>
2020-08-10 10:27:34 +0900

    build(Sample): add Player class in Test.cs

--- Sample/Assets/Test.cs
+++ Sample/Assets/Test.cs
@@ -2,13 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public class Player
+{
+    private int hp = 100;
+    private int power = 50;
+
+    public void Attack()
+    {
+        Debug.Log("Dealt " + this.power + " damage");
+    }
+
+    public void Damage(int damage)
+    {
+        this.hp -= damage;
+        Debug.Log("Received " + damage + " damage");
+    }
+}
+
 public class Test : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        // Print the "Hello, World" on the Console
-        Debug.Log("Hello, World");
+        Player myPlayer = new Player();
+        myPlayer.Attack();
+        myPlayer.Damage(30);
     }
 
     // Update is called once per frame
